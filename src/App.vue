685d17849092436df4888ea0b9a03496f04326f9
qiaofuu <1642896449@qq.com>
2019-10-11 09:17:32 +0800

    第一次

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav>
+    <router-link to="/" tag="span">首页</router-link>
+    <router-link to="/about" tag="span">医疗</router-link>
+    </nav>
+    
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data(){
+      return{
+            list:[
+              {
+                img:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1548509200,3423898172&fm=26&gp=0.jpg",
+                title:"中国"
+              },
+                {
+                img:"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3295174058,1884872095&fm=26&gp=0.jpg",
+                title:"美国"
+              },
+              {
+                img:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2083352004,97320740&fm=26&gp=0.jpg",
+                title:"德国"
+              }
+            ] 
+      }
+  }
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+</script>
+<style lang="scss">
+*{
+  margin:0;
+  padding:0; 
+} 
+html,body{
+  width:100%;
+  height:100%; 
+}
+#app{
+  width:100%;
+  height:100%;
+  overflow: hidden;
+  display:flex;
+  flex-direction:column;
+  nav{
+     width: 100%;
+     height: 50px;
+     display: flex; 
+     span{
+      flex:1;
+      text-align:center;
+      line-height:50px;
+     justify-content: space-between;
+     }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <div v-for="(item,index) in list" :key="index">
+          <swiper :options="swiperOption">
+           <swiper-slide><img :src="item.img" alt=""></swiper-slide>
+
+
+          </swiper>
+      </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data(){
+      return{
+            list:[
+              {
+                img:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1548509200,3423898172&fm=26&gp=0.jpg",
+                title:"中国"
+              },
+                {
+                img:"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3295174058,1884872095&fm=26&gp=0.jpg",
+                title:"美国"
+              },
+              {
+                img:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2083352004,97320740&fm=26&gp=0.jpg",
+                title:"德国"
+              }
+            ],
+            swiperOption:{
+                  autoplay:true,
+                  loop:true
+            }
+
+      }
   }
 }
 </script>
+<style lang="scss" scoped>
+ swiper{
+  width: 100%;
+  height: 200px;
+ 
+  swiper-slide{
+    width: 100%;
+    height: 200px; 
+    img{
+      width: 100%;
+      height: 200px;
+      overflow: hidden;
+    }
+  }
+ }
+
+
+</style>
